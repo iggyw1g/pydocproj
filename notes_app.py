99d051bf31e45786ce11079ad119f7a46bade63c
Alisa <146712422+iggyw1g@users.noreply.github.com>
2025-01-10 21:00:35 +0900

    added

--- notes_app.py
+++ notes_app.py
@@ -1,4 +1,20 @@
 from tkinter import *
+from tkinter import filedialog
+
+def quitapp(event=None):
+    root.destroy()
+
+def openfile():
+    file_path = filedialog.askopenfilename(
+        title="Open File",
+        filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
+    if file_path:
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        text_area.insert(1.0, content)
+
+
 
 root = Tk()
 root.title('Text Editor')
@@ -12,9 +28,14 @@ filemenu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label='File',menu=filemenu)
 #menubar.add_cascade(label='Save', menu=filemenu)
 
-filemenu.add_command(label='Open', accelerator='Control+O')
+filemenu.add_command(label='Open', accelerator='Control+O', command=openfile)
 filemenu.add_command(label='Save', accelerator='Control+S')
 
 filemenu.add_separator()
-filemenu.add_command(label='Exit', accelerator='Ctrl+Q')
+filemenu.add_command(label='Exit', accelerator='Ctrl+Q', command=quitapp)
+root.bind('<Control-q>', quitapp)
+
+text_area = Text(root, wrap="word")
+text_area.pack(expand=True, fill="both")
+
 root.mainloop()
